e8dc26570d78f7237f73d4fce7b44c2de9ed08b6
Sarah Harvey <sarahcharvey@hotmail.com>
2023-11-19 11:51:34 +0000

    started api retrieval, beginning by stating location details

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchedArea = document.getElementById('searchLocation');
 const submitLocation = document.getElementById('submitLocation');
-const resultsArea = document.getElementById('weatherResults')
+const resultsArea = document.getElementById('weatherResults');
 
 const weatherApiKey = '303a084ecffd4c6d868113603231311';
 
@@ -12,9 +12,36 @@ function search() {
 submitLocation.addEventListener('click', function() {
     const weatherLocation = search();
     searchedArea.value = '';
+    resultsArea.textContent = '';
 
-    const locationHeader = document.createElement('h2');
-    locationHeader.textContent = weatherLocation;
-    resultsArea.appendChild(locationHeader);
+    async function getWeather() {
+        try {
+            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${weatherLocation}&aqi=no`, { mode: 'cors'})
+            const weatherData = await response.json();
 
-})
+            console.log(weatherData);
+            
+            const resultLocation = document.createElement('p');
+            resultLocation.textContent = weatherData.location.name;
+            resultsArea.appendChild(resultLocation);
+
+            const resultRegion = document.createElement('p');
+            resultRegion.textContent = weatherData.location.region;
+            resultsArea.appendChild(resultRegion);
+
+            const resultCountry =  document.createElement('p');
+            resultCountry.textContent = weatherData.location.country;
+            resultsArea.appendChild(resultCountry);
+
+            const localTime = document.createElement('p')
+            localTime.textContent = weatherData.location.localtime;
+            resultsArea.appendChild(localTime);
+
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            
+        }
+    }
+
+    getWeather();
+});
